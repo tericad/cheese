a98c9906c5053965f3949f99f29b15b28642d4af
Terica <tericadonnelly@gmail.com>
2017-11-18 10:42:36 -0600

    switched to cheese objects

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -32,10 +33,34 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+        Cheese myCheese = new Cheese(cheeseName, cheeseDescription);
+        cheeses.add(myCheese);
 
         //Redirect to /cheese
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheese) {
+        ArrayList<Cheese> cheesesDelete = new ArrayList<>();
+
+        for (Cheese item : cheeses){
+            if (cheese.contains(item.getName())){
+                cheesesDelete.add(item);
+            }
+        }
+
+        for (Cheese item : cheesesDelete){
+            cheeses.remove(item);
+        }
         return "redirect:";
     }
 }
---
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -0,0 +1,29 @@
+package org.launchcode.cheesemvc.models;
+
+public class Cheese {
+
+    private String name;
+    private String description;
+
+    public Cheese(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String aDescription) {
+        description = aDescription;
+    }
+
+}
