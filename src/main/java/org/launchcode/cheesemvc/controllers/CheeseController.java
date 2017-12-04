6ba2f3ece1344bca1b9b511f4ba134d388d0cd1d
Terica <tericadonnelly@gmail.com>
2017-12-04 14:27:03 -0600

    added validation to cheese and rating in class 9 prep

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -2,11 +2,16 @@ package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
 import org.launchcode.cheesemvc.models.CheeseData;
+import org.launchcode.cheesemvc.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+
+
 
 @Controller
 @RequestMapping("cheese")
@@ -27,14 +32,21 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute Cheese newCheese){
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese,
+                                        Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Add Cheese");
+            return "cheese/add";
+        }
         CheeseData.add(newCheese);
 
-        //Redirect to /cheese
+        //Redirect to /
         return "redirect:";
     }
 
@@ -56,20 +68,31 @@ public class CheeseController {
     }
 
     @RequestMapping(value="/edit/{cheeseId}", method = RequestMethod.GET)
-    public String displayEditForm(Model model, @PathVariable int cheeseId){
+    public String displayEditForm(@ModelAttribute Cheese thisCheese, Model model,
+                                  @PathVariable int cheeseId){
         Cheese editCheese = CheeseData.getById(cheeseId);
         model.addAttribute("cheese", editCheese);
         model.addAttribute("title", "Edit Cheese");
+        model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/edit";
 
     }
 
     @RequestMapping(value="edit/{cheeseId}", method = RequestMethod.POST)
-    public String processEditForm(int cheeseId, String name, String description){
-        Cheese editCheese = CheeseData.getById(cheeseId);
-        editCheese.setName(name);
-        editCheese.setDescription(description);
+    public String processEditForm(@ModelAttribute @Valid Cheese editCheese,
+                                  Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Edit Cheese");
+            return "cheese/edit";
+        }
+        //   int cheeseId, String name, String description, type){
+        Cheese originalCheese = CheeseData.getById(editCheese.getCheeseId());
+        originalCheese.setName(editCheese.getName());
+        originalCheese.setDescription(editCheese.getDescription());
+        originalCheese.setType(editCheese.getType());
+        originalCheese.setRating(editCheese.getRating());
         return "redirect:/cheese/";
+
 
     }
 }
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,9 +1,24 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.validation.constraints.*;
+
 public class Cheese {
 
+    @NotNull
+    @Size(min=3, max=15)
     private String name;
+
+    @NotNull
+    @Size(min=1,message = "Description must not be empty")
     private String description;
+
+    private CheeseType type;
+
+    @NotNull
+    @Max(5)
+    @Min(1)
+    private Integer rating;
+
     private int cheeseId;
     private static int nextId = 1;
 
@@ -40,5 +55,21 @@ public class Cheese {
 
     public void setCheeseId(int cheeseId) {
         this.cheeseId = cheeseId;
+    }
+
+    public CheeseType getType(){
+        return type;
+    }
+
+    public void setType(CheeseType type) {
+        this.type = type;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
     }
 }
