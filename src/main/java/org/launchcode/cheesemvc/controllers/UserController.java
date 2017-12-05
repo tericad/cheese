bba95dd0094ee9b9dc717b45d2a72820370ad365
Terica <tericadonnelly@gmail.com>
2017-12-04 19:34:54 -0600

    added password validation on user add per studio from class 9. Did not to bonus

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -4,9 +4,12 @@ import org.launchcode.cheesemvc.models.CheeseData;
 import org.launchcode.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -14,22 +17,29 @@ public class UserController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model, @ModelAttribute User user) {
-
-        model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new User());
+        model.addAttribute("title", "New User");
 
         return "user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify){
-      if(user.getPassword().equals(verify)){
+    public String add(Model model, @ModelAttribute @Valid User user,
+                      Errors errors, String verify){
 
-          return "user/index";
-      }
-      else{
-
-          model.addAttribute("problem", "Password and Verify Password did not match.");
-          return "user/add";
-      }
+        if(errors.hasErrors()){
+            model.addAttribute(user);
+            model.addAttribute("title", "New User");
+            return "user/add";
+        }
+        if(user.getPassword().equals(verify)){
+            return "user/index";
+        }
+        else{
+            model.addAttribute(user);
+            model.addAttribute("title", "New User");
+            model.addAttribute("problem", "Password and Verify Password did not match.");
+            return "user/add";
+        }
     }
 }
--- src/main/java/org/launchcode/cheesemvc/models/User.java
+++ src/main/java/org/launchcode/cheesemvc/models/User.java
@@ -1,8 +1,22 @@
 package org.launchcode.cheesemvc.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotNull
+    @Size(min=5, max=15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+  @Email
     private String email;
+
+  @NotNull
+  @Size(min=6)
     private String password;
 
     public User(String username, String email, String password) {
